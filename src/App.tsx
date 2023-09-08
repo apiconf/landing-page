4b470739ffba290c59f70274d77c26efeb613ffe
Mubaraq Mustapha <50276258+mubaraqqq@users.noreply.github.com>
2023-09-08 15:43:32 +0100

    feat: implement footer (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="w-full bg-white">
-    <Header />
-    <Hero />
-    <main></main>
-    <footer></footer>
-  </div>
+      <Header />
+      <Hero />
+      <main></main>
+      <footer></footer>
+    </div>
   );
 }
 
