e75245c18d28b44e81cc72acb25d48f904e9748e
Odumuyiwa Teslim <teslimodumuyiwa@gmail.com>
2023-09-08 17:01:19 +0100

    Feat: The goal of API conf section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Body from "./components/About/Card";
 
 function App() {
   return (
