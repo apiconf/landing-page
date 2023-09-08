969326e598a88fe9c7639c5442b65caaee66bc00
Elishaokon13 <eliscodavid@gmail.com>
2023-09-08 17:24:15 +0100

    created Sponsor and register section solving issue #5

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Register from "./components/Cards";
+import Sponsor from "./components/Sponsor";
+
 
 function App() {
   return (
     <div className="w-full bg-white">
       <Header />
       <Hero />
+      <Register />
+      <Sponsor />
       <main></main>
       <Footer />
     </div>
