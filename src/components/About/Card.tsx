6976a423f0225f11ac981862a2a74240284f5c33
Tes-program <teslimodumuyiwa@gmail.com>
2023-09-08 15:51:51 +0100

    Responsive Loading...

--- src/components/About/Card.tsx
+++ src/components/About/Card.tsx
@@ -7,7 +7,7 @@ const Body = () => {
     <>
       <body className="bg-custom-blue">
         <div className="relative">
-          <p className="absolute bottom-0 top-0 text-4xl text-[color:#E2EF9C] px-28 pt-24">
+          <p className="absolute text-[color:#E2EF9C] text-lg font-medium py-10 px-10 sm:text-lg md:text-lg lg:text-xl xl:text-4xl 2xl:text-4xl lg:px-28 lg:py-24">
             The goal of <b>API Conf Lagos</b> is to promote API literacy amongst
             the different developer ecosystems that exist within Lagos, and in
             the future, Nigeria/Africa regardless of your expertise or skill
@@ -23,15 +23,15 @@ const Body = () => {
 const Card = () => {
   return (
     <body className="bg-custom-blue">
-        <div className="px-10 pb-8">
-        <div className="bg-[color:white] rounded-2xl flex flex-col justify-end items-start self-stretch gap-4 p-4">
-            <h1 className="self-stretch text-[color:#4334D3] text-3xl font-sans font-bold">
+        <div className="px-10 pt-[160px] lg:pt-10 pb-8">
+        <div className="bg-[color:white] rounded-2xl flex lg:flex-col justify-end items-start self-stretch gap-2 lg:gap-4 p-6">
+            <h1 className=" text-[color:#4334D3] text-2xl lg:text-3xl font-bold">
             API Conf Lagos is Tailored For You
             </h1>
-            <div className=" flex items-start gap-6 self-stretch">
+            <div className=" flex items-start max-[450px]:flex-col gap-6 self-stretch">
                 <div className="flex p-6 rounded-2xl shadow-lg self-stretch gap-4 flex-1 flex-col">
                     <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] text-2xl font-mono font-extrabold px-4 py-5 right-0 bottom-0 left-0 top-0">Experienced</h1>
+                    <h1 className="absolute text-[color:white] text-2xl font-extrabold px-4 py-5 right-0 bottom-0 left-0 top-0">Experienced</h1>
                     <img src={rectangle} className="shrink-0" />
                     </div>
                     <p className="text-2xl font-medium "> Seasoned Professionals</p>
@@ -39,7 +39,7 @@ const Card = () => {
                 </div>
                 <div className="flex p-6 rounded-2xl shadow-2xl self-stretch gap-4 flex-1 flex-col">
                 <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] text-2xl font-mono font-extrabold pl-2 py-5 right-0 bottom-0 left-0 top-0">Foundational</h1>
+                    <h1 className="absolute text-[color:white] text-2xl font-extrabold pl-2 py-5 right-0 bottom-0 left-0 top-0">Foundational</h1>
                     <img src={rectangle} className="shrink-0" />
                     </div>
                     <p className="text-2xl font-medium "> Professionals with Foundational Understanding</p>
