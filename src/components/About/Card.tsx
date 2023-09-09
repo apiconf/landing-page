f7ca707532b987d2c3b52cf45ae029af1cc876e0
Tes-program <teslimodumuyiwa@gmail.com>
2023-09-09 13:48:17 +0100

    Responsive Loading...

--- src/components/About/Card.tsx
+++ src/components/About/Card.tsx
@@ -7,7 +7,7 @@ const Body = () => {
     <>
       <body className="bg-custom-blue">
         <div className="relative">
-          <p className="absolute text-[color:#E2EF9C] text-lg font-medium py-10 px-10 sm:text-lg md:text-lg lg:text-xl xl:text-4xl 2xl:text-4xl lg:px-28 lg:py-24">
+          <p className="absolute text-[color:#E2EF9C] font-medium py-10 px-10 max-[480px]:text-3xl md:text-4xl lg:text-4xl xl:text-4xl 2xl:text-4xl lg:px-28 lg:py-24">
             The goal of <b>API Conf Lagos</b> is to promote API literacy amongst
             the different developer ecosystems that exist within Lagos, and in
             the future, Nigeria/Africa regardless of your expertise or skill
@@ -32,7 +32,7 @@ const Card = () => {
             
                 <div className="flex p-6 rounded-2xl shadow-lg self-stretch gap-4 flex-1 flex-col">
                     <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] text-[28px] font-extrabold px-4 py-[14px] right-0 bottom-0 left-0 top-0">Experienced</h1>
+                    <h1 className="absolute text-[color:white] md:text-[23px] xl:text-[28px] font-extrabold px-4 max-[480px]:text-[27px] py-[14px] right-0 bottom-0 left-0 top-0">Experienced</h1>
                     <img src={rectangle} className="shrink-0 " />
                     </div>
                     <p className="text-2xl font-medium "> Seasoned Professionals</p>
@@ -40,13 +40,13 @@ const Card = () => {
                 </div>
                 <div className="flex p-6 rounded-2xl shadow-2xl self-stretch gap-4 flex-1 flex-col">
                 <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] text-[28px] font-extrabold px-4 py-[14px] right-0 bottom-0 left-0 top-0">Foundational</h1>
+                    <h1 className="absolute text-[color:white] max-[480px]:text-[26px]  xl:text-[28px] md:text-[23px] font-extrabold px-4 md:px-3 py-[14px] right-0 bottom-0 left-0 top-0">Foundational</h1>
                     <img src={rectangle} className="shrink-0" />
                     </div>
                     <p className="text-2xl font-medium "> Professionals with Foundational Understanding</p>
                     <p className="">Individuals keen on understanding APIs, with a foundational understanding of coding.</p>
                 </div>
-                <img className="shrink-0 gap-4 pt-6 max-[470px]:items-start flex max-[470px]:px-10" src={imageAbstract} alt="Trademarks" />
+                <img className="shrink-0 gap-4 pt-6 max-[480px]:items-start flex max-[480px]:px-10" src={imageAbstract} alt="Trademarks" />
         </div>
         </div>
         </div>
