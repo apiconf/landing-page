0c96fc6b343378f31a0f5d4d41de42d785f831d3
eliscodavid@gmail.com <eliscodavid@gmail.com>
2023-09-09 05:29:10 +0100

    Fix: Renamed "Cards.jsx" to "Register.jsx" and made some styling corrections to fot the design

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -12,8 +12,8 @@ const Register = () => {
     <div className='w-full grid lg:grid-cols-2 mt-10 mb-10 items-center justify-center gap-8 px-4 lg:px-16'>
       <div className='h-[270px] bg-[#4121CF]  rounded-3xl p-6  grid grid-cols-3 gap-4'>
         <div className='flex flex-col items-start col-span-2 justify-around'>
-          <h1 className='text-3xl font-semibold text-white'>Register to Save your Seat!</h1>
-          <p className='text-sm text-gray-300'>Professionals with experience in working with APIs in software systems.</p>
+          <h1 className='text-3xl font-semibold text-primary-white'>Register to Save your Seat!</h1>
+          <p className='text-sm text-[color:#E2EF9C]'>Professionals with experience in working with APIs in software systems.</p>
           <button className='btn flex items-center justify-center rounded-full w-[200px] bg-gradient-to-r from-[#ECC89D] to-[#E2EF9C] text-[#4121CF] font-semibold py-4 gap-2'> <div className='bg-[#4121CF] p-1 rounded'><img src={ticket} alt="" /></div> Register to Attend</button>
         </div>
         <div className='bg-[#4121CF] z-10'>
@@ -21,8 +21,8 @@ const Register = () => {
         </div>      </div>
       <div className='h-[270px] bg-[#4121CF]  rounded-3xl p-6 grid grid-cols-3 gap-4'>
         <div className='flex flex-col items-start col-span-2 justify-around'>
-          <h1 className='text-3xl font-semibold text-white'>Volunteer to Speak on a Topic</h1>
-          <p className='text-sm text-gray-300'>Professionals with experience in working with APIs in software systems.</p>
+          <h1 className='text-3xl font-semibold text-primary-white'>Volunteer to Speak on a Topic</h1>
+          <p className='text-sm text-[color:#E2EF9C]'>Professionals with experience in working with APIs in software systems.</p>
           <button className='btn flex items-center justify-center rounded-full w-[200px] bg-gradient-to-r from-[#ECC89D] to-[#E2EF9C] text-[#4121CF] font-semibold py-4 gap-2'> <div className='bg-[#4121CF] p-1 rounded'><img src={mic} alt="" /></div> Apply to Speak</button>
         </div>
         <div className='bg-[#4121CF] z-10'>
