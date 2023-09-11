5f2977e81d7656cb541e37545b800889f3281700
Mubaraq Mustapha <50276258+mubaraqqq@users.noreply.github.com>
2023-09-11 12:26:33 +0100

    Cleanup (#14)
    * fix: fixed issues with the hero section
    
    * fix:horizontal paddings across all sections
    
    ---------
    
    Co-authored-by: coded1guy <grggmmnl@gmail.com>

--- src/components/About/Card.tsx
+++ src/components/About/Card.tsx
@@ -7,7 +7,7 @@ const Body = () => {
     <>
       <body className="bg-custom-blue">
         <div className="relative">
-          <p className="absolute text-[color:#E2EF9C] font-medium py-10 px-10 max-[480px]:text-3xl md:text-4xl lg:text-4xl xl:text-4xl 2xl:text-4xl lg:px-28 lg:py-24">
+          <p className="absolute text-[color:#E2EF9C] font-medium py-10 px-[5%] sm:px-[7%] max-[480px]:text-3xl md:text-4xl lg:text-4xl xl:text-4xl 2xl:text-4xl lg:py-24">
             The goal of <b>API Conf Lagos</b> is to promote API literacy amongst
             the different developer ecosystems that exist within Lagos, and in
             the future, Nigeria/Africa regardless of your expertise or skill
@@ -23,33 +23,49 @@ const Body = () => {
 const Card = () => {
   return (
     <body className="bg-custom-blue">
-        <div className="px-10 pt-[160px] lg:pt-10 pb-8">
+      <div className="px-[5%] sm:px-[4%] pt-[160px] lg:pt-10 pb-8">
         <div className="bg-[color:white] rounded-2xl flex lg:flex-col flex-col justify-end items-start self-stretch gap-2 lg:gap-4 p-6">
-        <h1 className=" text-[color:#4334D3] text-2xl lg:text-3xl font-bold">
+          <h1 className=" text-[color:#4334D3] text-2xl lg:text-3xl font-bold">
             API Conf Lagos is Tailored For You
-            </h1>
-            <div className=" flex items-start max-[450px]:flex-col gap-6 self-stretch">
-            
-                <div className="flex p-6 rounded-2xl shadow-lg self-stretch gap-4 flex-1 flex-col">
-                    <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] md:text-[23px] xl:text-[28px] font-extrabold px-4 max-[480px]:text-[27px] py-[14px] right-0 bottom-0 left-0 top-0">Experienced</h1>
-                    <img src={rectangle} className="shrink-0 " />
-                    </div>
-                    <p className="text-2xl font-medium "> Seasoned Professionals</p>
-                    <p className="">Professionals with experience in working with APIs in software systems.</p>
-                </div>
-                <div className="flex p-6 rounded-2xl shadow-2xl self-stretch gap-4 flex-1 flex-col">
-                <div className="relative pt-4">
-                    <h1 className="absolute text-[color:white] max-[480px]:text-[26px]  xl:text-[28px] md:text-[23px] font-extrabold px-4 md:px-3 py-[14px] right-0 bottom-0 left-0 top-0">Foundational</h1>
-                    <img src={rectangle} className="shrink-0" />
-                    </div>
-                    <p className="text-2xl font-medium "> Professionals with Foundational Understanding</p>
-                    <p className="">Individuals keen on understanding APIs, with a foundational understanding of coding.</p>
-                </div>
-                <img className="shrink-0 gap-4 pt-6 max-[480px]:items-start flex max-[480px]:px-10" src={imageAbstract} alt="Trademarks" />
+          </h1>
+          <div className=" flex items-start max-[450px]:flex-col gap-6 self-stretch">
+            <div className="flex p-6 rounded-2xl shadow-lg self-stretch gap-4 flex-1 flex-col">
+              <div className="relative pt-4">
+                <h1 className="absolute text-[color:white] md:text-[23px] xl:text-[28px] font-extrabold px-4 max-[480px]:text-[27px] py-[14px] right-0 bottom-0 left-0 top-0">
+                  Experienced
+                </h1>
+                <img src={rectangle} className="shrink-0 " />
+              </div>
+              <p className="text-2xl font-medium "> Seasoned Professionals</p>
+              <p className="">
+                Professionals with experience in working with APIs in software
+                systems.
+              </p>
+            </div>
+            <div className="flex p-6 rounded-2xl shadow-2xl self-stretch gap-4 flex-1 flex-col">
+              <div className="relative pt-4">
+                <h1 className="absolute text-[color:white] max-[480px]:text-[26px]  xl:text-[28px] md:text-[23px] font-extrabold px-4 md:px-3 py-[14px] right-0 bottom-0 left-0 top-0">
+                  Foundational
+                </h1>
+                <img src={rectangle} className="shrink-0" />
+              </div>
+              <p className="text-2xl font-medium ">
+                {" "}
+                Professionals with Foundational Understanding
+              </p>
+              <p className="">
+                Individuals keen on understanding APIs, with a foundational
+                understanding of coding.
+              </p>
+            </div>
+            <img
+              className="shrink-0 gap-4 pt-6 max-[480px]:items-start flex max-[480px]:px-10"
+              src={imageAbstract}
+              alt="Trademarks"
+            />
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </body>
   );
 };
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,11 +7,11 @@ import { ApplyBtn, VolunteerBtn } from "./heroBtn";
 
 const Hero = () => {
   return (
-    <section className="flex justify-center pt-[48px] pb-6xl px-[16px] lg:pt-[47.65px] lg:pb-[64px] lg:px-[64px]">
+    <section className="flex justify-center pt-[48px] pb-[23.68px] px-[5%] sm:px-[4%] lg:pt-[47.65px] lg:pb-[64px]">
       <div className="max-w-dsktp w-full flex flex-col gap-y-12">
         <div className="flex flex-col md:flex-row md:justify-between items-center gap-y-6 md:gap-x-6">
           <div className="max-w-[358px] md:max-w-[450px] lg:max-w-[600px] flex flex-col items-center md:items-start gap-y-4">
-            <button className="w-[163px] rounded-3xl py-3 bg-black flex justify-center items-center gap-x-2 shadow-timerBtn">
+            <button className="w-[163px] rounded-3xl py-3 bg-[#000] flex justify-center items-center gap-x-2 shadow-timerBtn">
               <span className="text-base font-extrabold text-[#F1F1F1]">
                 X Days to Go
               </span>
