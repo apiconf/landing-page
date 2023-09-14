04f3c815c8c598a47a90807ef3f76440db3ea2d0
Chukwuemeka Chukwurah <emekaemmanuel045@gmail.com>
2023-09-14 13:54:40 +0100

    Ensure days in the days to go section is dynamic

--- src/components/DaysToGo.tsx
+++ src/components/DaysToGo.tsx
@@ -1,10 +1,24 @@
+import {DateTime, Interval} from "luxon"
+
 import ticket from "../assets/ticket-blue-bg.svg";
 
+const calcDiff = ()=>{
+  const currentDateTime = DateTime.local().setZone("UTC+1");
+  const eventDateTime = DateTime.fromISO("2023-10-15T12:00:00").setZone(
+    "UTC+1",
+  );
+ if(currentDateTime >= eventDateTime) return "It's Today";
+  const interval = Interval.fromDateTimes(currentDateTime, eventDateTime);
+  const duration = Math.floor(interval.length("days"));
+  return duration === 1 ? "1 Day to Go" : duration+" Days to Go"
+}
+
 const DaysToGo = () => {
+  const diff = calcDiff();
   return (
     <section className="days-section py-[34px] sm:py-[105px] flex justify-center items-center sm:bg-center">
       <div className="flex flex-col text-[#FFFFFF] items-center gap-[5px]">
-        <h2 className="text-[45px] font-bold">44 Days to Go</h2>
+        <h2 className="text-[45px] font-bold">{diff}</h2>
         <p className="text-[24px] font-medium">
           14th October, 2023
           <span className="hidden sm:inline"> - Unilag Hall, Lagos***</span>
