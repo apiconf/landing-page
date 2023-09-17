83ccc907484c7bc05886c3d451b58b31c75edb36
coded1guy <grggmmnl@gmail.com>
2023-09-17 16:19:18 +0100

    fix: pixel perfection 2

--- src/components/About/Card.tsx
+++ src/components/About/Card.tsx
@@ -8,14 +8,14 @@ interface AboutCardProps {
 
 const AboutCard = ({ title, subTitle, info }: AboutCardProps) => {
   return (
-    <div className="bg-transparent max-w-[495px] p-[6.7%] sm:p-6 flex flex-col justify-center gap-y-4 text-primary-black rounded-3xl shadow-aboutCard">
+    <div className="bg-transparent max-w-[495px] md:max-w-full p-[6.7%] sm:p-6 md:flex-[1_1_495px] flex flex-col justify-center gap-y-4 text-primary-black rounded-3xl shadow-aboutCard">
       <h3 className="relative max-w-[192px] w-full h-[37px] flex justify-center items-center text-white text-center font-MonumentExtended-extrabold text-[18.414px] md:text-[16px] lg:text-[18.414px] leading-[1]">
         <img
           className="absolute inset-0 margin-auto w-full"
           src={BgAbout}
           alt=""
         />
-        <span className="z-10">{title}</span>
+        <span className="leading-tight z-10">{title}</span>
       </h3>
       <h4 className="text-[25px] font-bold">{subTitle}</h4>
       <p className="text-base font-medium opacity-80 leading-[110%]">{info}</p>
