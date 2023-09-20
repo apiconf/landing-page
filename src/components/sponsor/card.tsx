ff1438f645c0a7ec1ddd231d8becab8e50e9da57
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2023-09-20 12:30:22 +0100

    fix: sponsor section cards (#23)
    removed the max-width from the cards so it spreads to take up the available width

--- src/components/sponsor/card.tsx
+++ src/components/sponsor/card.tsx
@@ -11,7 +11,7 @@ const InquireContact = ({
   email,
 }: inquireContactProps) => {
   return (
-    <div className="max-w-full sm:max-w-[467px] w-full p-6 flex flex-col gap-2 items-center rounded-3xl bg-gradient-to-r from-[#ECC89D] to-[#E2EF9C] break-all">
+    <div className="max-w-full sm:max-w-[467px] md:max-w-full w-full p-6 md:flex-[1_1_467px] flex flex-col gap-2 items-center rounded-3xl bg-gradient-to-r from-[#ECC89D] to-[#E2EF9C] break-all">
       <h4 className="relative w-full max-w-[231px] w-full h-[37px] flex justify-center items-center text-custom-blue font-MonumentExtended-extrabold text-[18.414px] md:text-[16px] lg:text-[18.414px] leading-[1]">
         <img
           className="absolute inset-0 margin-auto w-full"
