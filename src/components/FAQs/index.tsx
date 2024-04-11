dcb90fab21b5499684a09fe9f8d26124112d8dc1
Olorunnishola Olamilekan <96974022+Keith-Web3@users.noreply.github.com>
2024-04-11 16:04:41 +0100

    feat: finished faqs v2 styling (#39)

--- src/components/FAQs/index.tsx
+++ src/components/FAQs/index.tsx
@@ -1,64 +1,77 @@
-import "./FAQs.css";
+import { motion } from 'framer-motion'
+import FaqItem from './FAQs'
+import { useState } from 'react'
 
-import FaqItem from "./FAQs";
-import decoration from "../../assets/faq-decoration.svg";
+const faqs = [
+  {
+    faqQuestion: 'What is API Conf Lagos?',
+    faqResponse:
+      'API Conf Lagos is a dynamic one-day event dedicated to promoting API literacy, gathering API enthusiasts, developers, and industry leaders in Lagos, Nigeria.',
+  },
+  {
+    faqQuestion: 'Who should attend API Conf Lagos?',
+    faqResponse:
+      "Anyone interested in APIs, whether you're an experienced developer, a beginner, or simply curious about APIs, is welcome. The event caters to both experts and newcomers.",
+  },
+  {
+    faqQuestion: 'What is the cost of attending?',
+    faqResponse:
+      'The event is absolutely free. Registration is now open. Be sure to sign up!',
+  },
+  {
+    faqQuestion: 'What topics will be covered at the conference?',
+    faqResponse:
+      'The conference will cover a wide range of topics related to APIs, their significance, use-cases, advancements, and the difference between various types like REST and GraphQL, among others.',
+  },
+  {
+    faqQuestion:
+      'Are there any accommodation recommendations for attendees from outside Lagos?',
+    faqResponse:
+      'We will provide a list of recommended hotels and accommodations closer to the event date. Attendees are encouraged to make their bookings in advance.',
+  },
+  {
+    faqQuestion: 'How can I register for the event?',
+    faqResponse:
+      'You can register for the event through the provided link on our social media pages and official website. Registration is now open, so go ahead and secure your spot! An official registration link is available for immediate sign-up.',
+  },
+  {
+    faqQuestion: 'Are there sponsorship opportunities available?',
+    faqResponse:
+      'Yes, we offer various sponsorship tiers. Interested sponsors can review our sponsorship deck for details or contact us directly.',
+  },
+  {
+    faqQuestion: 'Can I be a speaker at the event?',
+    faqResponse:
+      "We're always on the lookout for insightful speakers. If you're interested, please reach out to our team with your topic and a brief overview.",
+  },
+  {
+    faqQuestion:
+      'Where can I find updates and more information about the conference?',
+    faqResponse:
+      'Follow our official Twitter page (@apiconflagos) and other social media channels for real-time updates, announcements, and more details about the conference.',
+  },
+]
 
 const Faq = () => {
+  const [activeQues, setActiveQues] = useState(-1)
+
   return (
-    <div className="faq-section">
-      <div className="faq-title" id="faq">
-        <h1>Frequently Asked Questions</h1>
-        <img src={decoration} alt="" />
-      </div>
-
-      <div className="faq-columns">
-        <div>
+    <div className="py-8">
+      <motion.div
+        layout
+        className="bg-[#F7F7F7] p-4 w-[min(90%,_900px)] mx-auto rounded-3xl"
+      >
+        {faqs.map((faq, idx) => (
           <FaqItem
-            faqQuestion="What is API Conf Lagos?"
-            faqResponse="API Conf Lagos is a dynamic one-day event dedicated to promoting API literacy, gathering API enthusiasts, developers, and industry leaders in Lagos, Nigeria."
+            setActiveQues={setActiveQues}
+            activeQues={activeQues}
+            index={idx}
+            key={idx}
+            {...faq}
           />
-          <FaqItem
-            faqQuestion="Who should attend API Conf Lagos?"
-            faqResponse="Anyone interested in APIs, whether you're an experienced developer, a beginner, or simply curious about APIs, is welcome. The event caters to both experts and newcomers."
-          />
-          <FaqItem
-            faqQuestion="What is the cost of attending?"
-            faqResponse="The event is absolutely free. Registration is now open. Be sure to sign up!"
-          />
-          <FaqItem
-            faqQuestion="What topics will be covered at the conference?"
-            faqResponse="The conference will cover a wide range of topics related to APIs, their significance, use-cases, advancements, and the difference between various types like REST and GraphQL, among others."
-          />
-          <FaqItem
-            faqQuestion="Are there any accommodation recommendations for attendees from outside Lagos?"
-            faqResponse="We will provide a list of recommended hotels and accommodations closer to the event date. Attendees are encouraged to make their bookings in advance."
-          />
-        </div>
-
-        <div>
-          {/* <FaqItem
-            faqQuestion="When and where is the event taking place?"
-            faqResponse="The event is scheduled for October 14, 2023, in Lagos. The specific venue details will be provided closer to the event date."
-          /> */}
-          <FaqItem
-            faqQuestion="How can I register for the event?"
-            faqResponse="You can register for the event through the provided link on our social media pages and official website. Registration is now open, so go ahead and secure your spot! An official registration link is available for immediate sign-up."
-          />
-          <FaqItem
-            faqQuestion="Are there sponsorship opportunities available?"
-            faqResponse="Yes, we offer various sponsorship tiers. Interested sponsors can review our sponsorship deck for details or contact us directly."
-          />
-          <FaqItem
-            faqQuestion="Can I be a speaker at the event?"
-            faqResponse="We're always on the lookout for insightful speakers. If you're interested, please reach out to our team with your topic and a brief overview."
-          />
-          <FaqItem
-            faqQuestion="Where can I find updates and more information about the conference?"
-            faqResponse="Follow our official Twitter page (@apiconflagos) and other social media channels for real-time updates, announcements, and more details about the conference."
-          />
-        </div>
-      </div>
+        ))}
+      </motion.div>
     </div>
-  );
-};
-export default Faq;
+  )
+}
+export default Faq
