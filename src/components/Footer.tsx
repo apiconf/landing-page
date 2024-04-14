e8f1315074e7cd5a41df1b54c5fdbabe79d40c24
Olorunnishola Olamilekan <96974022+Keith-Web3@users.noreply.github.com>
2024-04-14 23:44:15 +0100

    feat: completed footers v2 styling (#40)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,103 +1,64 @@
-import email from "../assets/mail-icon.svg";
-import emailMobile from "../assets/email-blue-bg.svg";
-import linkedIn from "../assets/linkedIn-icon.svg";
-import linkedInMobile from "../assets/linkedIn-blue-bg.svg";
-import logo from "../assets/logo-white.svg";
-import play from "../assets/icon-play.svg";
-import x from "../assets/x-icon.svg";
-import xMobile from "../assets/x-blue-bg.svg";
+import image3D from '../assets/3D-Image.png'
+import conf2024 from '../assets/conf-2024.png'
+import arrow from '../assets/arrow-icon.svg'
 
 const Footer = () => {
   return (
-    <footer className="pt-[64px] px-[5%] sm:px-[4%] text-[#FFFFFF]">
-      <div className="bg-[#2F1C81] rounded-t-[24px] pt-[42px] pb-[42px] sm:pt-[82px] sm:pb-[4%] sm:px-[4%]">
-        {/* desktop / tab */}
-        <div className="hidden sm:flex items-start justify-between">
-          <div className="flex flex-col gap-[28px]">
-            <span className="flex gap-[14px] items-center">
-              <img src={email} alt="email" loading="lazy" />
-              <a
-                href="mailto:apiconferencelagos@gmail.com"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                apiconferencelagos@gmail.com
-              </a>
-            </span>
-            <span className="flex gap-[14px] items-center">
-              <img src={x} alt="x" loading="lazy" />
-              <a
-                href="https://x.com/apiconflagos/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Twitter
-              </a>
-            </span>
-            <span className="flex gap-[14px] items-center">
-              <img src={linkedIn} alt="linkedIn" loading="lazy" />
-              <a
-                href="https:www.linkedin.com/company/api-conf-lagos"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                linkedIn
-              </a>
-            </span>
-          </div>
-
-          <div className="flex flex-col w-[35%]">
-            <img src={logo} alt="apiconf" />
-            <p className="text-center mt-[64px]">2024 API Conf</p>
-          </div>
-
-          <div className="flex flex-col gap-[24px] items-end">
-            <img src={play} alt="play" width="57" height="57" />
-          </div>
-        </div>
-
-        {/* mobile */}
-        <div className="flex sm:hidden flex-col items-center justify-center gap-[40px]">
-          <div className="w-[60%]">
-            <img src={logo} alt="apiconf" />
-          </div>
-
-          <div className="flex flex-col gap-[20px]">
-            <span className="flex gap-[10px] items-center">
-              <img src={emailMobile} alt="email" loading="lazy" />
-              <a
-                href="mailto:info@apiconf.io"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                info@apiconf.io
-              </a>
-            </span>
-            <span className="flex gap-[10px] items-center">
-              <img src={xMobile} alt="x" loading="lazy" />
-              <a
-                href="https://x.com/apiconflagos/"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Twitter
-              </a>
-            </span>
-            <span className="flex gap-[10px] items-center">
-              <img src={linkedInMobile} alt="linkedIn" loading="lazy" />
-              <a
-                href="https://www.linkedin.com/company/api-conf-lagos"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                linkedIn
-              </a>
-            </span>
-          </div>
-        </div>
+    <footer className="bg-[#2F20BF] overflow-hidden relative grid grid-cols-[1fr,_max-content] grid-rows-[max-content,_1fr,_max-content] text-white capitalize pl-6 gap-y-8 gap-x-4 sm:grid-rows-[repeat(3,_auto)] sm:grid-cols-[max-content,_1fr] sm:gap-y-4 sm:gap-x-12 sm:p-4 lg:p-[4%]">
+      <h2 className="font-bold text-[3rem] font-sans leading-[1] text-end rotate-180 sm:rotate-0 sm:text-start sm:text-[5rem] sm:mb-4 md:text-8xl lg:text-9xl min-[1250px]:text-[10rem]">
+        API Conference
+      </h2>
+      <ul className="mt-12 mb-4 sm:mt-0 sm:mb-0">
+        s
+        <li className="flex items-center gap-2">
+          <a href="#">about API conference</a>
+          <img
+            className="w-4 rotate-[-45deg] opacity-80"
+            src={arrow}
+            alt="arrow-icon"
+          />
+        </li>
+        <li className="flex items-center gap-2">
+          <a href="#">something </a>
+          <img
+            className="w-4 rotate-[-45deg] opacity-80"
+            src={arrow}
+            alt="arrow-icon"
+          />
+        </li>
+        <li className="flex items-center gap-2">
+          <a href="#">something </a>
+          <img
+            className="w-4 rotate-[-45deg] opacity-80"
+            src={arrow}
+            alt="arrow-icon"
+          />
+        </li>
+        <li className="flex items-center gap-2">
+          <a href="#">speakers </a>
+          <img
+            className="w-4 rotate-[-45deg] opacity-80"
+            src={arrow}
+            alt="arrow-icon"
+          />
+        </li>
+      </ul>
+      <img
+        className="logo my-auto sm:my-0"
+        src={conf2024}
+        alt="api_conference_logo"
+      />
+      <div className="mb-12 sm:mb-0">
+        <p>made with love,</p>
+        <p>2024 API conference team</p>
       </div>
+      <img
+        src={image3D}
+        className="absolute w-[240px] inset-[auto_-45px_-30px_auto] lg:inset-[auto_150px_-10px_auto] lg:w-[300px]"
+        alt="api-conf"
+      />
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
