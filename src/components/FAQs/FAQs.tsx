2d3525adafcc9967f54280efab9fb69c5cea0ee8
Chukwuemeka Chukwurah <emekaemmanuel045@gmail.com>
2024-04-14 23:34:25 +0100

    use case sensitive relative paths in FAQs

--- src/components/FAQs/FAQs.tsx
+++ src/components/FAQs/FAQs.tsx
@@ -1,23 +1,22 @@
-import { motion } from 'framer-motion'
-
-import chevron from '../../Assets/chevron-icon.svg'
+import chevron from "../../assets/chevron-icon.svg";
+import { motion } from "framer-motion";
 
 interface faqItemProps {
-  faqQuestion: string
-  faqResponse: string
-  activeQues: number
-  index: number
-  setActiveQues: React.Dispatch<React.SetStateAction<number>>
+  faqQuestion: string;
+  faqResponse: string;
+  activeQues: number;
+  index: number;
+  setActiveQues: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const FaqItem = ({
-  faqQuestion = 'test question',
-  faqResponse = 'test answer',
+  faqQuestion = "test question",
+  faqResponse = "test answer",
   activeQues,
   setActiveQues,
   index,
 }: faqItemProps) => {
-  const itemOpen = index === activeQues
+  const itemOpen = index === activeQues;
 
   return (
     <motion.div
@@ -25,30 +24,30 @@ const FaqItem = ({
       className="cursor-pointer mb-4 last:mb-0 bg-white p-4 rounded-3xl select-none"
       onClick={() => {
         if (itemOpen) {
-          setActiveQues(-1)
-          return
+          setActiveQues(-1);
+          return;
         }
-        setActiveQues(index)
+        setActiveQues(index);
       }}
     >
       <div className="flex items-center justify-between mb-2 gap-4">
         <p className="font-bold text-lg">{faqQuestion}</p>
         <motion.img
-          initial={{ rotate: itemOpen ? '0' : '180deg' }}
-          animate={{ rotate: itemOpen ? '180deg' : '0' }}
-          transition={{ type: 'tween' }}
+          initial={{ rotate: itemOpen ? "0" : "180deg" }}
+          animate={{ rotate: itemOpen ? "180deg" : "0" }}
+          transition={{ type: "tween" }}
           src={chevron}
           alt="chevron"
         />
       </div>
       <motion.p
-        initial={{ height: itemOpen ? '0' : 'auto' }}
-        animate={{ height: itemOpen ? 'auto' : '0' }}
+        initial={{ height: itemOpen ? "0" : "auto" }}
+        animate={{ height: itemOpen ? "auto" : "0" }}
         className="text-lg overflow-hidden"
       >
         {faqResponse}
       </motion.p>
     </motion.div>
-  )
-}
-export default FaqItem
+  );
+};
+export default FaqItem;
