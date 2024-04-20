b48c031a061613b0db8346c69880e1c4e9d28d8d
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2024-04-20 20:15:00 +0100

    Hero section (#46)
    * feat: updated the hero feature
    
    * update: refactored the header based on the new design
    
    * update: added new assets and deleted old ones
    
    * feat: created a custom hook to animate based on its visibility in the current viewport
    
    * feat: added a context to know if the page has loaded completely and also wrapped it around the web app
    
    * update: deleted irrelevant components that were disturbing me :)
    
    * update: added framer motion for animation, fast marquee for the marquee and configured a new responsive breakpoint

---
+++ src/hooks/isVisible.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export type RefType = React.RefObject<HTMLDivElement | null>;
+
+export const useIsVisible = (ref: RefType) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const currentRef = ref.current;
+    if (currentRef) {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          setIsVisible(entry.isIntersecting);
+        },
+        { threshold: 0.5 }
+      );
+
+      observer.observe(currentRef);
+
+      return () => {
+        observer.unobserve(currentRef);
+      };
+    }
+  }, [ref]);
+
+  return isVisible;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        "3xl": "1650px",
+      },
       fontFamily: {
-        "MonumentExtended-extrabold": ["MonumentExtended-extrabold", "sans-serif"]
+        "MonumentExtended-extrabold": [
+          "MonumentExtended-extrabold",
+          "sans-serif",
+        ],
       },
       boxShadow: {
         timerBtn:
@@ -13,30 +19,30 @@ export default {
           "0px 0px 8px 0px rgba(47, 28, 129, 0.10), 0px 4px 32px 0px rgba(47, 28, 129, 0.15)",
         heroCard:
           "0px 0px 16px 0px rgba(47, 28, 129, 0.08), 0px 0px 2px 0px rgba(47, 28, 129, 0.12)",
-        aboutCard: 
-          "0px 0px 16px 0px rgba(47, 28, 129, 0.08), 0px 0px 2px 0px rgba(47, 28, 129, 0.12)"
+        aboutCard:
+          "0px 0px 16px 0px rgba(47, 28, 129, 0.08), 0px 0px 2px 0px rgba(47, 28, 129, 0.12)",
       },
       textColor: {
         "primary-black": "#1F1F1F",
-        "white": "#FFF",
+        white: "#FFF",
         gray: "#666",
-        "purple": "#4334D3",
+        purple: "#4334D3",
       },
       backgroundColor: {
-        "white": "#FFF",
+        white: "#FFF",
       },
       backgroundImage: {
         "lg-banner": "url('/src/assets/lg-banner.png')",
         "sm-banner": "url('/src/assets/sm-banner.png')",
         "lg-aboutSectionBg": "url('/src/assets/lg-about-section-bg.svg')",
-        "sm-aboutSectionBg": "url('/src/assets/sm-about-section-bg.svg')"
+        "sm-aboutSectionBg": "url('/src/assets/sm-about-section-bg.svg')",
       },
       borderRadius: {
         "6xl": "32px",
       },
     },
     colors: {
-      "custom-blue": "#4334D3"
+      "custom-blue": "#4334D3",
     },
     fontFamily: {
       sans: ["Cabinet-Grotesk"],
