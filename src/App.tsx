57e15afa5dabbc0c0e05ef23c137c32834632c6d
coded1guy <grggmmnl@gmail.com>
2024-04-26 14:25:10 +0100

    fix: resolved load issues, removed the aos package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import { useEffect } from "react";
-import AOS from "aos";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -11,10 +9,6 @@ import FAQs from "./components/FAQs";
 import Copyright from "./components/Copyright";
 
 function App() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
   return (
     <div className="w-full bg-white">
       <Header />
