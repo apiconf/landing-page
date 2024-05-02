edee06226e54fa6f6fb2be8fa4a9d9914fbaa4db
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2024-05-02 17:33:24 +0100

    refactor: updated the footer component (#53)
    * refactor: updated the footer component
    
    * refactor: removed irrelevant style from the index.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import FloatingBtn from "./components/floatingBtn";
 import About from "./components/About";
 import Talks from "./components/Talks";
-import Footer from "./components/Footer";
-import FloatingBtn from "./components/floatingBtn";
 import FAQs from "./components/FAQs";
-import Copyright from "./components/Copyright";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
         <FAQs />
       </main>
       <Footer />
-      <Copyright />
     </div>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import Copyright from "./Copyright";
+import FooterDetails from "./footerDetails";
+
+const Footer = () => {
+  return (
+    <footer>
+      <FooterDetails />
+      <Copyright />
+    </footer>
+  );
+};
+export default Footer;
