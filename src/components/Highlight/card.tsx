ee667c5da90252bb8ed4faa141d7db0c0a64fab5
coded1guy <grggmmnl@gmail.com>
2024-05-14 16:29:28 +0100

    fix: carousel navigation issues

--- src/components/Highlight/card.tsx
+++ src/components/Highlight/card.tsx
@@ -1,12 +1,24 @@
 import styles from "./css/style.module.css";
+import sampleImg from "../../assets/lg-about-section-bg.svg";
 
-function HiglightCard() {
+interface HighlightPicsProps {
+  id?: number;
+  url: string;
+  alt: string;
+}
+
+function HiglightCard({ url, alt }: HighlightPicsProps) {
   return (
     <div className={styles["embla__slide"]}>
       {/* Use an Image instead */}
-      <div className="h-[95%] w-full bg-[#000000] rounded-[64px] -rotate-2"></div>
+      <div className="h-[95%] w-full bg-[#000000] rounded-[64px] -rotate-2 relative">
+        <img
+          src={url ? url : sampleImg}
+          alt={alt ? alt : ""}
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   );
 }
-
 export default HiglightCard;
