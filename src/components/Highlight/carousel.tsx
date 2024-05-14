2e98aba1f4240cf84b30ee019e579bafb4055f85
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2024-05-14 17:31:40 +0100

    fix: carousel navigation issues (#66)

--- src/components/Highlight/carousel.tsx
+++ src/components/Highlight/carousel.tsx
@@ -1,7 +1,6 @@
 import { EmblaOptionsType } from "embla-carousel";
-import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import HiglightCard from "./card";
 import styles from "./css/style.module.css";
 
@@ -12,28 +11,79 @@ type PropType = {
 
 const Carousel: React.FC<PropType> = (props) => {
   const { slides, options } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
-    Autoplay({ playOnInit: false, delay: 3000 }),
-  ]);
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [span, setSpan] = useState(0);
+  // keyboard gesture - left and right arrow button click
+  const keyHandler = useCallback(
+    (e: KeyboardEvent) => {
+      if (!emblaApi) return;
+      if (e.key === "ArrowLeft") {
+        emblaApi.scrollPrev();
+      } else if (e.key === "ArrowRight") {
+        emblaApi.scrollNext();
+      }
+    },
+    [emblaApi]
+  );
+  // mouse gesture - swipe horizontally
+  const swipeHandler = useCallback(
+    (e: WheelEvent) => {
+      if (!emblaApi) return;
 
-  const toggleAutoplay = useCallback(() => {
-    const autoplay = emblaApi?.plugins()?.autoplay;
-    if (!autoplay) return;
+      // track only horizontal wheel event
+      if (Math.abs(e.deltaX) < Math.abs(e.deltaY)) {
+        return;
+      }
 
-    const playOrStop = autoplay.play as any;
-    playOrStop();
-  }, [emblaApi]);
+      // reduce the span if you swipe to the right, increase the span if you swipe to the left
+      if (e.deltaX > 0) {
+        setSpan((num) => (num -= 1));
+      } else {
+        setSpan((num) => (num += 1));
+      }
 
+      if (Math.abs(span) < 5) return;
+
+      if (span > 0) {
+        emblaApi.scrollPrev();
+      } else if (span < 0) {
+        emblaApi.scrollNext();
+      }
+
+      setSpan(0);
+    },
+    [emblaApi, span]
+  );
+  // tracks the keyboard handler
   useEffect(() => {
-    toggleAutoplay();
-  }, []);
+    window.addEventListener("keydown", keyHandler);
+
+    return () => {
+      window.removeEventListener("keydown", keyHandler);
+    };
+  }, [keyHandler]);
+  // tracks the mouse swipe handler
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    const containerNode = emblaApi.containerNode();
+    if (!containerNode) return;
+
+    containerNode.addEventListener("wheel", swipeHandler);
+
+    return () => {
+      containerNode.removeEventListener("wheel", swipeHandler);
+    };
+  }, [swipeHandler]);
+
+  const data = { url: "", alt: "" };
 
   return (
     <div className={styles.embla}>
       <div className={styles["embla__viewport"]} ref={emblaRef}>
         <div className={styles["embla__container"]}>
           {slides.map((index) => (
-            <HiglightCard key={index} />
+            <HiglightCard key={index} {...data} id={index} />
           ))}
         </div>
       </div>
