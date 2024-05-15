eaf1ff3987f647cdeec3b749bd24fc19de051f36
coded1guy <grggmmnl@gmail.com>
2024-05-15 13:45:56 +0100

    chore: updated the highlight images

---
+++ src/hooks/getSlides.tsx
@@ -0,0 +1,35 @@
+import image1 from "../assets/highlight/image1.jpg";
+import image2 from "../assets/highlight/image2.jpg";
+import image3 from "../assets/highlight/image3.jpg";
+import image4 from "../assets/highlight/image4.jpg";
+import image5 from "../assets/highlight/image5.jpg";
+import image6 from "../assets/highlight/image6.jpg";
+import image7 from "../assets/highlight/image7.jpg";
+import image8 from "../assets/highlight/image8.jpg";
+import image9 from "../assets/highlight/image9.jpg";
+import image10 from "../assets/highlight/image10.jpg";
+import image11 from "../assets/highlight/image11.jpg";
+import image12 from "../assets/highlight/image12.jpg";
+import image13 from "../assets/highlight/image13.jpg";
+import image14 from "../assets/highlight/image14.jpg";
+
+const useGetSlides = () => {
+  const slides = [
+    { id: 1, url: image1, alt: "" },
+    { id: 2, url: image2, alt: "" },
+    { id: 3, url: image3, alt: "" },
+    { id: 4, url: image4, alt: "" },
+    { id: 5, url: image5, alt: "" },
+    { id: 6, url: image6, alt: "" },
+    { id: 7, url: image7, alt: "" },
+    { id: 8, url: image8, alt: "" },
+    { id: 9, url: image9, alt: "" },
+    { id: 10, url: image10, alt: "" },
+    { id: 11, url: image11, alt: "" },
+    { id: 12, url: image12, alt: "" },
+    { id: 13, url: image13, alt: "" },
+    { id: 14, url: image14, alt: "" },
+  ];
+  return slides;
+};
+export default useGetSlides;
