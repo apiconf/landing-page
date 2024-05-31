cd5beebd59905586b6a3d193b806bae76b99e737
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-05-31 12:11:36 +0100

    refactor: Update Confirmation component layout for better responsiveness

--- src/components/Confirmation/index.tsx
+++ src/components/Confirmation/index.tsx
@@ -4,7 +4,7 @@ import ConfirmationFooter from "./confirmation-footer";
 export default function Confirmation() {
   return (
     <div className="bg-dark-purple min-h-screen w-full">
-      <div className="w-[90%] xl:w-4/5 mx-auto py-32">
+      <div className="w-3/4 md:w-[90%] xl:w-4/5 mx-auto py-32">
         <ConfirmationMain />
         <ConfirmationFooter />
       </div>
