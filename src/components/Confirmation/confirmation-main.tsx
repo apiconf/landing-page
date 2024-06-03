9bc16047aea7d01e619be33fce32d395970b9be4
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-06-03 10:48:08 +0100

    feat: new poster

--- src/components/Confirmation/confirmation-main.tsx
+++ src/components/Confirmation/confirmation-main.tsx
@@ -217,7 +217,7 @@ function ImagePreviewContainer({
           <img
             src={imageSource}
             alt=""
-            className="bg-white rounded-3xl mt-9 md:mt-0 w-[45%] sm:w-1/2 md:w-[42.5%] lg:w-52 aspect-square object-cover object-center"
+            className="bg-white rounded-3xl md:mt-0 w-[35%] md:w-[39.5%] aspect-square object-cover object-center"
           />
           <p className={Styles.ImagePreviewName}>{nameState}</p>
         </div>
