1386a9551a0d9c1b2595cca4213689c992a8bf51
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-07-15 11:45:53 +0100

    feat: Update sessions page

--- src/pages/sessions/index.tsx
+++ src/pages/sessions/index.tsx
@@ -1,21 +1,8 @@
-import React from "react";
+// import { motion } from "framer-motion";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import ScrollToAnchor from "../../components/ScrollToAnchor";
-// import { motion } from "framer-motion";
 import ColonIcon from "../../assets/colon.svg";
 import styles from "./sessions.module.css";
-
-export default function Sessions() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-      <ScrollToAnchor />
-    </>
-  );
-}
 
 type sessions = {
   key?: number;
@@ -24,34 +11,169 @@ type sessions = {
   employer: string;
   sessionType: "WORKSHOP" | "PANEL SESSION" | "TALK";
   talkTitle: string;
+  image: string;
 };
 
 const cardData: sessions[] = [
   {
     key: 0,
-    speaker: "Ojonemi Joshua-Tamunoebiye",
-    job: "Developer Ecosystem Manager",
-    employer: "Microsoft",
+    speaker: "Cornelius Ashley-Osuzoka",
+    job: "Engineering Manager, Developer Experience",
+    employer: "Flutterwave Inc",
     sessionType: "WORKSHOP",
-    talkTitle: 'API Security: What does "data.data.data.value" mean??',
+    talkTitle: "Plug in and play: How to design SDKs to extend your APIs",
+    image: "/Cornelius-Ashley-Osuzoka.jpeg",
   },
   {
     key: 1,
-    speaker: "Ojonemi Joshua-Tamunoebiye",
-    job: "Developer Ecosystem Manager",
-    employer: "Microsoft",
+    speaker: "Adeyeye Adedoyinsolami",
+    job: "Product Manager",
+    employer: "Revent Technologies",
     sessionType: "PANEL SESSION",
-    talkTitle: 'API Security: What does "data.data.data.value" mean??',
+    talkTitle: "How Product Management Principles Drive API Strategy",
+    image: "/Adeyeye-Adedoyinsolami.jpg",
   },
   {
     key: 2,
-    speaker: "Ojonemi Joshua-Tamunoebiye",
-    job: "Developer Ecosystem Manager",
-    employer: "Microsoft",
+    speaker: "Funke Olasupo",
+    job: "Technical Writer",
+    employer: "",
     sessionType: "TALK",
-    talkTitle: 'API Security: What does "data.data.data.value" mean??',
+    talkTitle:
+      "From Design to Documentation: Building Interactive OpenAPI Specification",
+    image: "/Funke-Olasupo.jpg",
+  },
+  {
+    key: 3,
+    speaker: "David Ijaola",
+    job: "Product Manager",
+    employer: "Norebase",
+    sessionType: "WORKSHOP",
+    talkTitle: "Pivoting Internal APIs to Revenue-Generating Products",
+    image: "/David-Ijaola.jpeg",
+  },
+  {
+    key: 4,
+    speaker: "Ridwan Kasim",
+    job: "Senior Backend Engineer",
+    employer: "SeamlessHR",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Ensuring API Compliance: Best Practices and Strategies",
+    image: "/Ridwan-Kasim.jpg",
+  },
+  {
+    key: 5,
+    speaker: "Fawaz Haroun",
+    job: "Software Engineer",
+    employer: "Paystack",
+    sessionType: "PANEL SESSION",
+    talkTitle: "API Performance Monitoring with Prometheus and Grafana",
+    image: "/Fawaz-Haroun.jpg",
+  },
+  {
+    key: 6,
+    speaker: "Uchechukwu Azubuko",
+    job: "Lead Frontend Engineer",
+    employer: "OneLiquidity",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Patterns for a Clean Frontend Application With Valid Data",
+    image: "/Uchechukwu-Azubuko.jpeg",
+  },
+  {
+    key: 7,
+    speaker: "Motunrayo Koyejo",
+    job: "Software Engineer",
+    employer: "Brass",
+    sessionType: "PANEL SESSION",
+    talkTitle: "API Versioning: Building for Longevity and Adaptability",
+    image: "/Motunrayo-Koyejo.jpg",
+  },
+  {
+    key: 8,
+    speaker: "Esther Okafor",
+    job: "Quality assurance engineer",
+    employer: "Storyblok GMBH",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Beyond the Code: A QA Tester's Perspective on API Design",
+    image: "/Esther-Okafor.jpg",
+  },
+  {
+    key: 9,
+    speaker: "Rufai Mustapha",
+    job: "Technical Instructor",
+    employer: "Pluralsight",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Leveraging Open Banking APIs for Innovation",
+    image: "/Rufai-Mustapha.jpg",
+  },
+  {
+    key: 10,
+    speaker: "Tobiloba Adedeji",
+    job: "DevRel Engineer",
+    employer: "Fleek",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Browser APIs: Things You Need To Know",
+    image: "/Tobiloba-Adedeji.jpeg",
+  },
+  {
+    key: 11,
+    speaker: "Terhile ikyo",
+    job: "Senior Project Manager",
+    employer: "FCMB Group",
+    sessionType: "PANEL SESSION",
+    talkTitle:
+      "Metrics that Matter: Measuring the Success of Your API Strategy",
+    image: "/Terhile-ikyo.jpg",
+  },
+  {
+    key: 12,
+    speaker: "Okunoye David",
+    job: "Software Engineer",
+    employer: "TWEN",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Browser APIs: Things You Need To Know",
+    image: "/Okunoye-David",
+  },
+  {
+    key: 13,
+    speaker: "Bolaji Olajide",
+    job: "Software Engineer",
+    employer: "Sourcegraph",
+    sessionType: "PANEL SESSION",
+    talkTitle: "Rethinking API Security",
+    image: "/Bolaji-Olajide",
+  },
+  {
+    key: 14,
+    speaker: "Emmanuel Odianosen",
+    job: "Developer Support Engineer",
+    employer: "Fuse Blockchain",
+    sessionType: "PANEL SESSION",
+    talkTitle:
+      "Building Blockchain Applications using APIs:  A Practical Guide to Fuse Blockchain REST APIs",
+    image: "/Emmanuel-Odianosen",
+  },
+  {
+    key: 15,
+    speaker: "Kenechukwu Nwankwo",
+    job: "Senior Product Manager",
+    employer: "",
+    sessionType: "PANEL SESSION",
+    talkTitle:
+      "Navigating the Fog: Effective Product Discovery for API Products",
+    image: "/Kenechukwu-Nwankwo",
   },
 ];
+
+export default function Sessions() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
 
 function Main() {
   return (
@@ -80,18 +202,24 @@ function GridCard({
   employer,
   sessionType,
   talkTitle,
+  image,
 }: sessions) {
   return (
     <GridContainer>
-      <div className="flex justify-between md:gap-x-10 xl:gap-x-16">
-        <div className="flex items-center gap-x-8">
+      <div className="flex justify-center md:justify-between md:gap-x-10 xl:gap-x-16">
+        <div className="w-full flex flex-col lg:flex-row items-center gap-8">
           <div className="w-40 min-h-[12.5rem] shrink-0 rounded-2xl bg-[#4536DD]">
-            {/* Image Container */}
+            <img
+              src={image}
+              alt={speaker}
+              className="w-full h-full object-fill rounded-2xl"
+            />
           </div>
-          <div className="text-[#F1F1F1] font-sans">
+          <div className="text-[#F1F1F1] w-full text-center lg:text-left font-sans">
             <p className="text-2xl font-bold mb-2">{speaker}</p>
             <p className="text-lg font-medium">
-              {job} • {employer}
+              {job}
+              {employer.trim() === "" ? "" : ` • ${employer}`}
             </p>
           </div>
         </div>
@@ -103,13 +231,13 @@ function GridCard({
           height="43.847"
         />
       </div>
-      <div className="md:ml-10 xl:ml-16">
+      <div className="flex flex-col items-center md:items-start md:ml-10 xl:ml-16">
         <div className="my-4 rounded-[2rem] px-4 py-1 w-max bg-[#E1EF9A]">
           <span className="text-[#0A0A0A] font-sans text-lg font-bold">
             {sessionType}
           </span>
         </div>
-        <p className="text-[#F1F1F1] font-sans text-4xl xl:text-5xl font-bold">
+        <p className="text-[#F1F1F1] text-center md:text-left break-words font-sans text-4xl xl:text-5xl font-bold">
           {talkTitle}
         </p>
       </div>
