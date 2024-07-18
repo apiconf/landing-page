2cbb4ccf7b899d2bde2744f7306ac029aa9b7ec2
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2024-07-18 06:13:26 +0100

    Feat/speaker session in home (#104)
    * feat: added the speakers component to the home page
    
    * feat: added the sessions component to the home page
    
    * fix: sponsor button height was larger than the design intended
    
    * refactor: made requested changes to speakers section on homepage and speakers page
    
    * refactor: made requested changes to sessions section in homepage

---
+++ src/components/session/index.tsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useIsVisible } from "../../hooks";
+import { SessionGridForOutdoorUse } from "../../pages/sessions";
+
+export default function SessionsInHome() {
+  const buttonRef = useRef(null);
+  const isButtonVisible = useIsVisible(buttonRef, 0.75);
+  const buttonVariant = {
+    visible: {
+      y: isButtonVisible ? 0 : 32,
+      opacity: isButtonVisible ? 1 : 0,
+      transition: {
+        ease: [0, 0, 0.58, 1],
+        delay: 0.3,
+        y: { duration: 0.75 },
+        opacity: { duration: 1 },
+      },
+    },
+    hidden: {
+      y: 32,
+      opacity: 0,
+    },
+  };
+  return (
+    <section className="bg-[#2F20BF] min-h-screen px-12 xl:px-32 pt-10 xl:pt-16 pb-16 xl:pb-[108px]">
+      <SessionGridForOutdoorUse no={3} />
+      <motion.a
+        href="/sessions"
+        rel="noopener noreferrer"
+        target="_blank"
+        ref={buttonRef}
+        initial="hidden"
+        animate="visible"
+        variants={buttonVariant}
+        className="w-full max-w-[330px] lg:max-w-[466px] lg:w-[70.01%] max-h-[126px] h-full mt-6 mx-auto py-8 px-8 lg:py-12 cursor-pointer bg-[#E1EF9A] flex justify-center items-center rounded-[320px]"
+      >
+        <span className="text-black font-bold text-2xl leading-[29.76px]">
+          SEE FULL SCHEDULE
+        </span>
+      </motion.a>
+    </section>
+  );
+}
