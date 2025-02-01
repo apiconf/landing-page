5b79fc2ef1cb884d7c4df8836a6969e8f4f3c9bb
Chukwuemeka Chukwurah <emekaemmanuel045@gmail.com>
2025-02-02 00:58:54 +0100

    feat: add 2025 cfp link

--- src/components/2025/Footer.tsx
+++ src/components/2025/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full bg-[#1F1F1F]">
       <div className="w-full max-w-7xl mx-auto flex flex-col-reverse md:flex-row justify-between items-center text-white px-[8%] sm:px-[5%] 2xl:px-[7.4%] py-8 md:py-0">
         <p className="py-[30px] leading-[22.32px] text:base md:text-lg font-bold md:font-medium">
-          API Conference team &copy; 2025
+          API Conference Team &copy; 2025
         </p>
         <div className="flex gap-6 items-center text-white">
           <span className="font-bold">Connect:</span>
