18b9658ff473c23029e8200f0ce846cdc7563b1e
Eazyisreal <eazyisreal192@gmail.com>
2025-03-08 12:28:34 +0100

    chore: buid Sponsors, CFP and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
+
+import Footer from "./components/2025/Footer";
 import Header from "./components/2025/Header";
 import Main from "./components/2025/Main";
-import Footer from "./components/2025/Footer";
+import Sponsor from "./components/2025/sponsor_cfp/index"
 
 export default function App() {
   return (
     <div className="bg-dark-purple w-full min-h-[100svh] flex flex-col">
       <Header />
       <Main />
+      <Sponsor />
       <Footer />
     </div>
   );
---
+++ src/components/2025/sponsor_cfp/index.tsx
@@ -0,0 +1,32 @@
+import Card from './card';
+import { motion } from 'framer-motion';
+
+export default function Sponsor() {
+  return (
+    <section className="bg-white w-full justify-center flex py-20">
+      <motion.div
+        className="flex flex-col md:flex-row px-8 justify-center gap-[8rem]"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: 'easeOut' }}
+      >
+        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+          <Card
+            title="Sponsor APIConf Lagos 2025"
+            description="Learn more details to sponsor API Conference"
+            buttonText="View Sponsorship Deck"
+            additionalButtonClassName="bg-[#2F20BF] text-white"
+          />
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+          <Card
+            title="Speak at the 2025 Edition"
+            description="Speak at the 2025 edition of API conference Lagos!"
+            buttonText="Apply to Speak"
+            additionalButtonClassName="bg-[#ECC89D]"
+          />
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+}
