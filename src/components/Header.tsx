da055b125f37852812a9f729978adc02459a5e0b
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-03-12 16:54:01 +0100

    Events Section (#116)
    * feat: create types files
    
    * feat (spaces2024): added props, moved SpaceItem type to types.ts, move spacesArray to talks
    
    * feat (Talks2024): add spaceArray
    
    * feat: add Spaces section with upcoming API talks
    
    * feat: new image
    
    * feat: create events section
    
    * feat: remove 2025 folder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useIsVisible } from "../../hooks";
+import { useIsVisible } from "../hooks";
 import { useRef } from "react";
 
-export default function Header() {
+export default function NewHeader() {
   const headerRef = useRef(null);
   const isHeaderVisible = useIsVisible(headerRef, -1);
 
--- src/components/Talks/spaces/index.tsx
+++ src/components/Talks/spaces/index.tsx
@@ -8,113 +8,9 @@ import { useRef } from "react";
 import { motion } from "framer-motion";
 import { useIsVisible } from "../../../hooks";
 import SpacesCarousel from "./spaces-carousel";
+import { SpaceItem } from "../../../types";
 
-type SpaceItem = {
-  key: string;
-  title: string;
-  subTitle: string;
-  link: string;
-  iconOne?: string;
-  iconTwo?: string;
-  btnIcon?: string;
-  btnText?: string;
-};
-
-const spaceItems: SpaceItem[] = [
-  {
-    key: "Integrating_Fintech_SDKs_using_Fincra_as_a_case_study",
-    title: "Integrating Fintech SDKs, using Fincra as a case study",
-    subTitle: "@xandychris • 6th July 2024",
-    link: "https://x.com/i/spaces/1LyGBnXpelMGN",
-  },
-  {
-    key: "Scalable_API_Testing_for_Optimal_Performance",
-    title: "Scalable API Testing for Optimal Performance",
-    subTitle: "@OmoladeEkpeni • 29th June 2024",
-    link: "https://x.com/i/spaces/1mrGmyPDZovGy",
-  },
-  {
-    key: "Building_API_based_products_that_scale",
-    title: "Building API based products that scale",
-    subTitle: "@goodnesskayode • 15th June 2024",
-    link: "https://x.com/i/spaces/1rmxPMkvyknKN",
-  },
-  {
-    key: "Leveraging_APIs_for_Microservices_Architecture",
-    title: "Leveraging APIs for Microservices Architecture",
-    subTitle: "@hybeecodes • 1st June 2024",
-    link: "https://x.com/i/spaces/1vAxRvwpWOvxl",
-  },
-  {
-    key: "API_Versioning_and_Backward_Compatibility",
-    title: "API Versioning and Backward Compatibility.",
-    subTitle: "@horlaarsco • 25th May 2024",
-    link: "https://x.com/i/spaces/1BRKjPYNjbRJw",
-  },
-  {
-    key: "API_Security_Strategies_for_Protecting_Sensitive_Data",
-    title: "API Security: Strategies for Protecting Sensitive Data",
-    subTitle: "@PaschalDev • 18th May 2024",
-    link: "https://x.com/i/spaces/1lPKqbVRmqAGb",
-  },
-  {
-    key: "Best_Practices_for_Integrating_APIs_with_Mobile_Applications",
-    title: "Best Practices for Integrating APIs with Mobile Applications",
-    subTitle: "@MajorE_1 • 11th May 2024",
-    link: "https://x.com/i/spaces/1OyJAWpymdwKb",
-  },
-  {
-    key: "API_and_Webhooks",
-    title: "API & Webhooks",
-    subTitle: "@subomiOluwalana • 4th May 2024",
-    link: "https://x.com/i/spaces/1jMJgmOdMOmKL",
-  },
-  {
-    key: "API_Monitoring_and_Observability",
-    title: "API Monitoring and Observability",
-    subTitle: "@tonialaribe • 27th April 2024",
-    link: "https://x.com/i/spaces/1eaKbgVgblQGX",
-  },
-  {
-    key: "Web_API_Security_Risks",
-    title: "Web API Security Risks",
-    subTitle: "@bigdevlarry • 20th April 2024",
-    link: "https://x.com/i/spaces/1gqxvQploXOJB",
-  },
-  {
-    key: "Best_Practices_for_Testing_APIs_Effectively",
-    title: "Best Practices for Testing APls Effectively",
-    subTitle: "@chidi_godwn • 13th April 2024",
-    link: "https://x.com/i/spaces/1MYxNojoXkyKw",
-  },
-  {
-    key: "AMA_Session_With_The_Team",
-    title: "AMA Session With The Team",
-    subTitle: "@oluwabamikemi • 6th April 2024",
-    link: "https://x.com/i/spaces/1lPKqbwmnNNGb",
-  },
-  {
-    key: "Best_Practices_for_API_Error_Handling",
-    title: "Best Practices for API Error Handling",
-    subTitle: "@lucciddev • 30th March 2024",
-    link: "https://x.com/i/spaces/1vOxwjvoOvmJB",
-  },
-  {
-    key: "Best_Practices_for_writing_good_API_Documentation",
-    title: "Best Practices for writing good API Documentation",
-    subTitle: "@Anita_ihuman • 23th March 2024",
-    link: "https://x.com/i/spaces/1vAxRvggRrPxl",
-  },
-  {
-    key: "API_Literacy_101_What_should_developers_know_about_APIs_that_they_don't",
-    title:
-      "API Literacy 101: What should developers know about APIs that they don't?",
-    subTitle: "@theshalvah & @chukwurah__ • 27th January 2024",
-    link: "https://x.com/i/spaces/1RDxllOoRPMxL",
-  },
-];
-
-const Spaces = () => {
+const Spaces = ({ spaces }: { spaces: SpaceItem[] }) => {
   const bgSpacesMobileRef = useRef(null);
   const bgSpacesRef = useRef(null);
   const spacesInnerContainerRef = useRef(null);
@@ -166,7 +62,7 @@ const Spaces = () => {
         className="w-full py-32 md:pt-[12.091%] md:pb-[9.8154%] 3xl:pt-[178px] 3xl:pb-[144.5px] relative z-50 flex md:flex-row flex-col justify-center gap-8"
       >
         <SpacesCarousel>
-          {spaceItems.map((item) => (
+          {spaces.map((item) => (
             <Space
               key={item.key}
               title={item.title}
