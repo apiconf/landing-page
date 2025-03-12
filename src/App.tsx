4864a1af25feba745920e881a3c331129571ebb4
Emmanuel Olubiyi <grggmmnl@gmail.com>
2025-03-12 23:32:59 +0100

    feat: implemented the recap design

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/2025/Header";
 import Main from "./components/2025/Main";
 import Footer from "./components/2025/Footer";
+import Recap from "./components/2025/recap";
 
 export default function App() {
   return (
     <div className="bg-dark-purple w-full min-h-[100svh] flex flex-col">
       <Header />
       <Main />
+      <main>
+        <Recap />
+      </main>
       <Footer />
     </div>
   );
