8a5a6032158e903e119696e983543ea3810c4645
Emmanuel Olubiyi <grggmmnl@gmail.com>
2025-03-13 09:04:46 +0100

    chore: added some recommended rabbit changes

--- src/components/recap.tsx
+++ src/components/recap.tsx
@@ -1,4 +1,5 @@
 import Marquee from "react-fast-marquee";
+import bannerImg from "../../../src/assets/recap.png";
 
 export default function Recap() {
     return (
@@ -6,8 +7,8 @@ export default function Recap() {
             <div className="w-full py-16 px-[5.9701%] md:px-[7.4074%] flex flex-col items-center gap-y-8">
                 <h2 className="font-bold recap-fluid-heading">2024 Recap</h2>
                 <div className="relative w-full vid-h-fluid overflow-hidden border-2 border-solid border-custom-blue rounded-[3rem]">
-                <img src="../../../src/assets/recap.png" className="w-full h-full object-cover bg-[radial-gradient(circle,_transparent_0%,_#90EAF2_100%)]" />
-                <a className="w-[133px] h-[62px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] no-underline bg-[#E1EF9A] rounded-[2rem] cursor-pointer flex justify-center items-center text-2xl font-bold text-[#000000]">Watch</a>
+                <img src={bannerImg} alt="" className="w-full h-full object-cover bg-[radial-gradient(circle,_transparent_0%,_#90EAF2_100%)]" />
+                <a className="w-[133px] h-[62px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] no-underline bg-[#E1EF9A] rounded-[2rem] cursor-pointer flex justify-center items-center text-2xl font-bold text-[#000000]" href="">Watch</a>
                 </div>
             </div>
             <div className="w-full py-16 flex flex-col gap-y-8">
