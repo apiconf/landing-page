d66a5a11c48a3ecc45e765e71da77d31c19e93c9
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-04-14 13:27:54 +0100

    style(sponsors): adjust main section top padding
    - Reduce top padding from pt-52 to pt-48 on mobile
    - Reduce lg:pt-60 to lg:pt-52 on larger screens

--- src/pages/sponsors/main.tsx
+++ src/pages/sponsors/main.tsx
@@ -2,7 +2,7 @@ export default function Main() {
   return (
     <main className="flex w-full flex-1">
       <div className="min-h-full w-full flex-1">
-        <div className="relative mx-auto flex w-full flex-col justify-between gap-12 px-4 pb-8 pt-52 md:top-[2%] md:w-[85%] md:flex-row md:gap-0 md:gap-x-[clamp(1rem,7%,3rem)] md:px-0 lg:top-[6%] lg:pt-60 xl:top-[10%]">
+        <div className="relative mx-auto flex w-full flex-col justify-between gap-12 px-4 pb-8 pt-48 md:top-[2%] md:w-[85%] md:flex-row md:gap-0 md:gap-x-[clamp(1rem,7%,3rem)] md:px-0 lg:top-[6%] lg:pt-52 xl:top-[10%]">
           <div className="flex-1 md:max-w-[640px]">
             <h1 className="text-balance font-sans text-4xl font-bold leading-[100%] tracking-[-0.045rem] text-primary-black sm:text-5xl md:text-[3.5rem] md:tracking-[-0.08rem] lg:text-[4rem]">
               API Conference Lagos,
