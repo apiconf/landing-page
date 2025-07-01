09495e9f704a9f863f2962703cc7fb010464387b
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-01 10:27:25 +0100

    refactor: removed unneeded main element

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,6 @@ export default function App() {
         <NavBar />
         <Hero />
       </header>
-      <main></main>
       <WhyAttend />
       <Recap />
       <Spaces spaces={spaceItems} />
