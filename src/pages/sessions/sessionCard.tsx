4736c9c6f6af80d61792e65db1de20c0d5764938
eazyisreal <105181077+Eazyisreal@users.noreply.github.com>
2025-07-11 07:29:33 +0100

    Chat Widget Integration with Ndu (#142)
    * fix: added scroll to top
    
    * Chore: Added contact us section
    
    * chore: Built session page
    
    * fix: Fix build issue
    
    * fix: Fix build issue
    
    * fix: Fixed Session Type
    
    * fix: Fixed Page title and Route
    
    * fix: Fixed Workshop color and Async Api slot
    
    * fix: Fix build issue
    
    * feat: Added Workshops
    
    * Chore: Updated schedule and added code of conduct
    
    * Chore: Updated schedule and added code of conduct
    
    * Fix: Remove Target Blank from Schedule button
    
    * Chore: Updated Event schedule
    
    * fix: AsyncAPI speakers
    
    * feat: Added APICONF agent chatbot
    
    * fix: Fixed build issues
    
    * fix: Updated Schedule
    
    * fix: Updated Schedule
    
    * fix: Updated Schedule with panelist

--- src/pages/sessions/sessionCard.tsx
+++ src/pages/sessions/sessionCard.tsx
@@ -14,6 +14,7 @@ export const SessionCard = ({
       className={`flex flex-col gap-[9px] rounded-[.75rem] py-6 px-4 ${extraTopPadding ? 'mt-8' : ''}`}
       style={{ backgroundColor: session.color }}
     >
+      {/* Top metadata row */}
       <div className="flex w-full items-center justify-between">
         <div className="flex gap-3 bg-white rounded-[3.25rem] py-1 px-2">
           <p className="text-[#6E6E6E] text-[.625rem] whitespace-nowrap uppercase md:text-sm">
@@ -33,12 +34,26 @@ export const SessionCard = ({
       </div>
 
       <p className="font-bold text-sm md:text-base">{session.title}</p>
-      {session.speaker && (
-        <p className="font-bold text-sm md:text-base text-gray">{session.speaker}</p>
+
+      {(session.speaker || session.host || session.panelist) && (
+        <div className="flex flex-col lg:flex-row lg:justify-between gap-1 lg:gap-0">
+          {session.speaker && (
+            <p className="font-bold text-sm md:text-base text-gray-700">
+              {session.speaker}
+            </p>
+          )}
+          {session.host && (
+            <p className="font-bold text-[.625rem] md:text-base text-gray-700">
+              Host: {session.host}
+            </p>
+          )}
+          {session.panelist && (
+            <p className="font-bold text-[.65rem] md:text-base text-gray-700">
+              Panelist: {session.panelist}
+            </p>
+          )}
+        </div>
       )}
     </div>
   );
 };
-
-
-
