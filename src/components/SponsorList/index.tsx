4898779f0f85545cf46a9f534e612f6dcb6bd1ae
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-11 10:11:18 +0100

    Sponsor-announcement/alat-pay (#144)
    * feat: comment out AlatPay
    
    * style: Gold is ranked higher than Silver

--- src/components/SponsorList/index.tsx
+++ src/components/SponsorList/index.tsx
@@ -11,7 +11,7 @@ type Sponsors = {
 }[];
 
 const sponsors: Sponsors = [
-  // { name: 'ALATPay', logo: '/sponsors/alat-pay.png', tier: 'Gold' },
+  { name: 'ALATPay', logo: '/sponsors/alat-pay.png', tier: 'Gold' },
   { name: 'Google for Developers', logo: '/sponsors/Google-for-Developers.svg', tier: 'Silver' },
   { name: 'MyCover AI', logo: '/sponsors/mycover-ai.png', tier: 'Silver' },
   { name: 'Interswitch', logo: '/sponsors/Interswitch.png', tier: 'Bronze' },
